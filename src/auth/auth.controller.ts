961112e1e7f91f48796ceafbac0722052b85ec0c
phamtienanh <phamtienanh157@gmail.com>
2022-10-06 15:14:47 +0700

    authentication feature

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { IAccessToken } from './types/access-token.interface';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  async signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.authService.signUp(authCredentialsDto);
+  }
+
+  @Post('/signin')
+  async signIn(
+    @Body() authCredentialsDto: AuthCredentialsDto,
+  ): Promise<IAccessToken> {
+    return this.authService.signIn(authCredentialsDto);
+  }
+}
